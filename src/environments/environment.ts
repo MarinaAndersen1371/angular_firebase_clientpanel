be26a61e790d8e2937df3d84b9866b083c5d2ea9
MarinaAndersen1371 <72156348+MarinaAndersen1371@users.noreply.github.com>
2022-03-03 23:00:22 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyAW12FF-Ov_gcxosf2ilsGk755fTBPzSIw',
-    authDomain: 'marangularapp.firebaseapp.com',
-    projectId: 'marangularapp',
-    storageBucket: 'marangularapp.appspot.com',
-    messagingSenderId: '667225138887',
-    appId: '1:667225138887:web:346d0597dde51957d1c0e2',
+  
   },
 };
 
